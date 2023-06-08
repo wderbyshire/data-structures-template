8192ed81aceb9bda5691419195e76dc0a2111d69
bilbo <bilbo.derbyshire@gmail.com>
2023-06-08 13:13:52 +0100

    Fix: Pointer and size calculation

--- queues.py
+++ queues.py
@@ -6,33 +6,34 @@ check_full, check_size
 new_queue = [None, None, None, None, None, None]
 
 front_pointer = 0
-end_pointer = 0
+back_pointer = -1
+current_size = 0
 
 # Add an item to a queue (enqueue)
 new_value = "John Benson"
-new_queue[end_pointer] = new_value
-end_pointer += 1
+back_pointer += 1
+new_queue[back_pointer] = new_value
+current_size += 1
 
 # Enqueue
 new_value = "Ben Johnson"
-new_queue[end_pointer] = new_value
-end_pointer += 1
+back_pointer += 1
+new_queue[back_pointer] = new_value
+current_size += 1
 
 # Read item from the front of the queue (dequeue)
 dequeued_item = new_queue[front_pointer]
 front_pointer += 1
-
-# Find size of the queue
-size_of_queue = end_pointer - front_pointer
+current_size -= 1
 
 # Check if queue is empty
-if size_of_queue == 0:
+if current_size == 0:
     print("Queue is empty")
 else:
     print("Queue is not empty")
 
 # Check if queue is full
-if end_pointer - 1 == len(new_queue):
+if current_size == len(new_queue):
     print("Queue is full")
 else:
     print("Queue is not full")
